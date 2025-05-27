87a75b53484034cd840f97244f05dde039f85ff1
alejamart12 <mamartineg@eafit.edu.co>
2025-05-27 17:34:23 -0500

    :)

--- proyecto_nodo/cursos/urls.py
+++ proyecto_nodo/cursos/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UsuarioViewSet, CursoViewSet
+from . import views
 
 router = DefaultRouter()
 router.register(r'usuarios', UsuarioViewSet)
 router.register(r'cursos', CursoViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router.urls)),  
+    path('login/', views.login_view, name='login'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('profesor-dashboard/', views.profesor_dashboard, name='profesor_dashboard'),
+    path('estudiante-dashboard/', views.estudiante_dashboard, name='estudiante_dashboard'),
 ]
--- proyecto_nodo/cursos/views.py
+++ proyecto_nodo/cursos/views.py
@@ -2,6 +2,9 @@
 from rest_framework import viewsets
 from .models import Usuario, Curso
 from .serializers import UsuarioSerializer, CursoSerializer
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
@@ -10,3 +13,34 @@ class UsuarioViewSet(viewsets.ModelViewSet):
 class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        contraseña = request.POST['contraseña']
+        try:
+            usuario = Usuario.objects.get(email=email)
+            if usuario.contraseña == contraseña:
+                request.session['usuario_id'] = usuario.id_nodo
+                request.session['rol'] = usuario.rol
+                # Redirigir según el rol
+                if usuario.rol == 'Administrador':
+                    return redirect('admin_dashboard')
+                elif usuario.rol == 'Profesor':
+                    return redirect('profesor_dashboard')
+                else:
+                    return redirect('estudiante_dashboard')
+            else:
+                return render(request, 'login.html', {'error': 'Contraseña incorrecta'})
+        except Usuario.DoesNotExist:
+            return render(request, 'login.html', {'error': 'Usuario no encontrado'})
+    return render(request, 'login.html')
+
+def admin_dashboard(request):
+    return HttpResponse("Bienvenido al panel de administrador")
+
+def profesor_dashboard(request):
+    return HttpResponse("Bienvenido al panel de profesor")
+
+def estudiante_dashboard(request):
+    return HttpResponse("Bienvenido al panel de estudiante")
